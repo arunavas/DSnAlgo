77c9ca1ddebd8e8fb0b22a0edcdddcec82439142
Arunava Saha <arunavasaha@live.com>
2018-06-14 00:53:48 +0530

    Added a new type synonym to increase readability

--- 06-11-2018/arunava.hs
+++ 06-11-2018/arunava.hs
@@ -5,6 +5,7 @@ type Board = [[Int]]
 type Index = (Int, Int)
 type Size = (Int, Int)
 type Cost = Int
+type Path = [Position]
 
 data Direction = L | R | U | D deriving (Show, Enum, Bounded)
 data Position = Position { getCost :: Cost, getPos :: Index }
@@ -13,7 +14,7 @@ instance Show Position where
 instance Eq Position where
     (Position c1 (x1, y1)) == (Position c2 (x2, y2)) = x1 == x2 && y1 == y2
 
-guardPos :: Cost -> [Position] -> [Position] -> Position -> [Position]
+guardPos :: Cost -> [Position] -> Path -> Position -> Path
 guardPos _ _ [] _ = []
 guardPos cost ps pp@((Position c pi):_) cp@(Position _ (x', y')) = case find (==cp) pp of
          Nothing -> case find (==cp) ps of
@@ -27,11 +28,11 @@ move (Position c (x, y)) R = Position c (x, y + 1)
 move (Position c (x, y)) U = Position c (x - 1, y)
 move (Position c (x, y)) D = Position c (x + 1, y)
 
-nextMoves :: Cost -> [Position] -> [Position] -> [Position]
+nextMoves :: Cost -> [Position] -> Path -> Path
 nextMoves _ _ [] = []
 nextMoves cost ps pps@(curPos:_) = map (move curPos) [L .. D] >>= guardPos cost ps pps
 
-shortestPath :: Cost -> [Position] -> [[Position]] -> [[Position]]
+shortestPath :: Cost -> [Position] -> [Path] -> [Path]
 shortestPath _ _ [] = []
 shortestPath c ps acc = case partition (\(p:_) -> getCost p == c) acc of
              ([], rest) -> shortestPath c ps $ rest >>= \l -> nextMoves c ps l >>= \x -> return (x:l)
@@ -40,7 +41,7 @@ shortestPath c ps acc = case partition (\(p:_) -> getCost p == c) acc of
 convert :: Board -> [Position]
 convert = (\(x, l) -> map (\(y, c) -> Position c (x, y)) l) <=< zip [0..] . map (zip [0..])
 
-solution :: Int -> Board -> [[Position]]
+solution :: Int -> Board -> [Path]
 solution _ [] = []
 solution cost b = case convert b of
              [] -> []
